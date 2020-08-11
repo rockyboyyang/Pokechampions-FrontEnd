4803c7048e5b9659b8c5789a95e85c4b30f28dfa
Rocky <yangrocky2013@gmail.com>
2020-08-11 12:53:20 -0500

    add pokemon names and sprites api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
 import Login from "./components/views/Login";
 import Signup from "./components/views/Signup";
@@ -13,10 +13,38 @@ const App = props => {
   const backendUrl = "http://localhost:5000"
   const [tokenState, setToken] = useState(localStorage.access_token);
   const [user, setUser] = useState(localStorage.user)
+  const [pokemonList, setPokemonList] = useState([])
+  const [spritesList, setSpritesList] = useState({})
+  // Grabs a list of Pokemon
+  const fetchPokemonNames = async () => {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
+
+    const { results } = await res.json();
+    setPokemonList(results)
+    fetchPokemonSprites(results)
+  }
+
+  const fetchPokemonSprites = async (pokemonList) => {
+    let sprites = {}
+
+    let i = 0;
+    while(i < pokemonList.length) {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonList[i].name}`);
+      const results = await res.json()
+      const realSprites = results.sprites.versions['generation-vii']['ultra-sun-ultra-moon']
+      sprites[pokemonList[i].name] = realSprites
+      i++
+    }
+    setSpritesList(sprites)
+  }
+  
+  useEffect(() => {
+    fetchPokemonNames();
+  }, [])
 
   return (
     <BrowserRouter>
-      <AppContext.Provider value={{ backendUrl, setToken, setUser, user }}>
+      <AppContext.Provider value={{ backendUrl, setToken, setUser, user, pokemonList, spritesList }}>
         <Switch>
           <Route path="/signup" component={Signup}/>
           <Route path="/login" component={Login} />
--- src/components/views/SelectTeam.js
+++ src/components/views/SelectTeam.js
@@ -1,19 +1,28 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
 import { AppContext } from '../../context/AppContext'
 import Navbar from '../Navbar'
 import Footer from '../Footer'
 
 const SelectTeam = () => {
-    return (
-        <div className="view-body">
-            <Navbar />
-            <div className="center-body">
-                <div className="left-box">Select Team</div>
-                <div className="right-box"></div>
+    const { pokemonList, spritesList } = useContext(AppContext)
+
+    if(Object.keys(spritesList).length) {
+        return (
+            <div className="view-body">
+                <Navbar />
+                <div className="center-body">
+                <div className="left-box pokemon-list">{pokemonList.map((pokemon) => (
+                    <div><p>{pokemon.name}</p> <img src={spritesList[pokemon.name].front_default}></img></div>
+                ))}</div>
+                    <div className="right-box"></div>
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+        )
+    }
+    return (
+        <h1 className="loading-screen">Loading...</h1>
     )
 }
 
