27ec2c2a96e5e9413e070bf15d39162abc169dd7
Rocky <yangrocky2013@gmail.com>
2020-09-23 20:18:44 -0500

    change backendurl

--- src/backendURL.js
+++ src/backendURL.js
@@ -1,4 +1,4 @@
-// const backendUrl = "https://infinite-everglades-82990.herokuapp.com"
-const backendUrl = "http://localhost:5000"
+const backendUrl = "https://infinite-everglades-82990.herokuapp.com"
+// const backendUrl = "http://localhost:5000"
 
 export default backendUrl
